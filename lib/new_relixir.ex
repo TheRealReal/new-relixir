225e223128c71214be863516b98c448338beba7b
David Cuddeback <david.cuddeback@gmail.com>
2015-12-22 16:56:20 -0800

    define application startup

--- lib/new_relixir.ex
+++ lib/new_relixir.ex
@@ -1,2 +1,40 @@
 defmodule NewRelixir do
+  @moduledoc """
+  Entry point for New Relixir OTP application.
+  """
+
+  use Application
+
+  @doc """
+  Application callback to start New Relixir.
+  """
+  @spec start(Application.app, Application.start_type) :: :ok | {:error, term}
+  def start(_type \\ :normal, _args \\ []) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(:statman_server, [1000]),
+      worker(:statman_aggregator, []),
+    ]
+
+    opts = [strategy: :one_for_one, name: NewRelixir.Supervisor]
+    result = Supervisor.start_link(children, opts)
+
+    :ok = :statman_server.add_subscriber(:statman_aggregator)
+
+    if (app_name = Application.get_env(:new_relixir, :application_name)) && (license_key = Application.get_env(:new_relixir, :license_key)) do
+      Application.put_env(:newrelic, :application_name, to_char_list(app_name))
+      Application.put_env(:newrelic, :license_key, to_char_list(license_key))
+
+      {:ok, _} = :newrelic_poller.start_link(&:newrelic_statman.poll/0)
+    end
+
+    result
+  end
+
+  @doc false
+  @spec configured? :: boolean
+  def configured? do
+    Application.get_env(:new_relixir, :application_name) != nil && Application.get_env(:new_relixir, :license_key) != nil
+  end
 end
--- mix.exs
+++ mix.exs
@@ -10,23 +10,14 @@ defmodule NewRelixir.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [mod: {NewRelixir, []},
+     applications: [:logger, :lhttpc]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:phoenix, "~> 1.1"},
+     {:ecto, "~> 1.1"},
+     {:newrelic, git: "https://github.com/wooga/newrelic-erlang.git"}]
   end
 end
