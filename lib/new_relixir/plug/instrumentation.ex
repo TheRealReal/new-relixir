c9cda7308a23f2000b69041b257d598b323ca64e
David Cuddeback <david.cuddeback@gmail.com>
2016-03-18 16:20:18 -0700

    adds Plug-based instrumentation module

---
+++ lib/new_relixir/plug/instrumentation.ex
@@ -0,0 +1,92 @@
+defmodule NewRelixir.Plug.Instrumentation do
+  @moduledoc """
+  Utility methods for instrumenting parts of an Elixir app.
+  """
+
+  @doc """
+  Instruments a database call and records the elapsed time.
+
+  * `conn` should be a `Plug.Conn` that has been configured by `NewRelixir.Plug.Phoenix`.
+  * `action` is the name of the repository method being instrumented.
+  * `queryable` is the `Queryable` being passed to the repository.
+
+  By default, the query name will be infered from `queryable` and `action`. This can be overriden
+  by providing a `:query` option in `opts`.
+  """
+  @spec instrument_db(atom, Ecto.Queryable.t, Keyword.t, fun) :: any
+  def instrument_db(action, queryable, opts, f) do
+    {elapsed, result} = :timer.tc(f)
+
+    opts
+    |> put_model(queryable)
+    |> put_action(action)
+    |> record(elapsed)
+
+    result
+  end
+
+  defp put_model(opts, queryable) do
+    case Keyword.fetch(opts, :model) do
+      {:ok, _} -> opts
+      :error ->
+        if model = infer_model(queryable) do
+          Keyword.put(opts, :model, model)
+        else
+          opts
+        end
+    end
+  end
+
+  defp put_action(opts, action) do
+    Keyword.put_new(opts, :action, action)
+  end
+
+  defp infer_model(%{__struct__: model_type, __meta__: %Ecto.Schema.Metadata{}}) do
+    model_name(model_type)
+  end
+
+  defp infer_model(%Ecto.Changeset{model: model}) do
+    infer_model(model)
+  end
+
+  defp infer_model(%Ecto.Query{from: {_, model_type}}) do
+    model_name(model_type)
+  end
+
+  defp infer_model(%Ecto.Query{}) do
+    nil
+  end
+
+  defp infer_model(queryable) do
+    infer_model(Ecto.Queryable.to_query(queryable))
+  end
+
+  defp model_name(model_type) do
+    model_type
+    |> inspect
+    |> String.split(".")
+    |> List.last
+  end
+
+  defp record(opts, elapsed) do
+    if (conn = Keyword.get(opts, :conn)) && (transaction = Map.get(conn.private, :new_relixir_transaction)) do
+      NewRelixir.Transaction.record_db(transaction, get_query(opts), elapsed)
+    end
+  end
+
+  defp get_query(opts) do
+    case Keyword.fetch(opts, :query) do
+      {:ok, value} ->
+        value
+
+      :error ->
+        case {Keyword.fetch(opts, :model), Keyword.fetch(opts, :action)} do
+          {{:ok, model}, {:ok, action}} ->
+            {model, action}
+
+          _ ->
+            "SQL"
+        end
+    end
+  end
+end
---
+++ test/new_relixir/plug/instrumentation_test.exs
@@ -0,0 +1,93 @@
+defmodule NewRelixir.Plug.InstrumentationTest do
+  use ExUnit.Case, async: false
+  import TestHelpers.Assertions
+  import Plug.Conn
+  require Ecto.Query
+
+  alias NewRelixir.Plug.Instrumentation
+
+  defmodule FakeModel do
+    use Ecto.Schema
+    schema "fake_models" do
+    end
+  end
+
+  @transaction_name "TestTransaction"
+
+  setup do
+    conn = %Plug.Conn{}
+    |> put_private(:new_relixir_transaction, NewRelixir.Transaction.start(@transaction_name))
+
+    :ok = :statman_histogram.init
+
+    {:ok, conn: conn}
+  end
+
+  # query names
+
+  test "instrument_db records elapsed time with correct key when given custom query string", %{conn: conn} do
+    Instrumentation.instrument_db(:foo, %Ecto.Query{}, [conn: conn, query: "TestQuery"], fn -> nil end)
+    assert_contains(:statman_histogram.keys, {@transaction_name, {:db, "TestQuery"}})
+  end
+
+  test "instrument_db infers query name from instance of Ecto model and action name", %{conn: conn} do
+    Instrumentation.instrument_db(:foo, %FakeModel{}, [conn: conn], fn -> nil end)
+    assert_contains(:statman_histogram.keys, {@transaction_name, {:db, "FakeModel.foo"}})
+  end
+
+  test "instrument_db infers query name from module for Ecto model and action name", %{conn: conn} do
+    Instrumentation.instrument_db(:foo, FakeModel, [conn: conn], fn -> nil end)
+    assert_contains(:statman_histogram.keys, {@transaction_name, {:db, "FakeModel.foo"}})
+  end
+
+  test "instrument_db infers query name from Ecto changeset and action name", %{conn: conn} do
+    changeset = Ecto.Changeset.cast(%FakeModel{}, %{}, [], [])
+    Instrumentation.instrument_db(:foo, changeset, [conn: conn], fn -> nil end)
+    assert_contains(:statman_histogram.keys, {@transaction_name, {:db, "FakeModel.foo"}})
+  end
+
+  test "instrument_db infers query name from query on an Ecto model", %{conn: conn} do
+    query = Ecto.Query.from(FakeModel)
+    Instrumentation.instrument_db(:foo, query, [conn: conn], fn -> nil end)
+    assert_contains(:statman_histogram.keys, {@transaction_name, {:db, "FakeModel.foo"}})
+  end
+
+  test "instrument_db records query as SQL when query name can't be determined", %{conn: conn} do
+    Instrumentation.instrument_db(:foo, %Ecto.Query{}, [conn: conn], fn -> nil end)
+    assert_contains(:statman_histogram.keys, {@transaction_name, {:db, "SQL"}})
+  end
+
+  # with transaction
+
+  test "instrument_db records accurate elapsed time", %{conn: conn} do
+    {_, elapsed_time} = :timer.tc(fn ->
+      Instrumentation.instrument_db(:foo, %Ecto.Query{}, [conn: conn], fn ->
+        :ok = :timer.sleep(42)
+      end)
+    end)
+
+    [{recorded_time, _}] = :statman_histogram.get_data({@transaction_name, {:db, "SQL"}})
+    assert_between(recorded_time, 42000, elapsed_time)
+  end
+
+  test "instrument_db returns value of instrumented function", %{conn: conn} do
+    return_value = Instrumentation.instrument_db(:foo, %Ecto.Query{}, [conn: conn], fn ->
+      42
+    end)
+    assert return_value == 42
+  end
+
+  # with no transaction
+
+  test "instrument_db does not record elapsed time when transaction is not present" do
+    Instrumentation.instrument_db(:foo, %Ecto.Query{}, [conn: %Plug.Conn{}], fn -> nil end)
+    assert Enum.empty?(:statman_histogram.keys)
+  end
+
+  test "instrument_db returns value of instrumented function when transaction is not present" do
+    return_value = Instrumentation.instrument_db(:foo, %Ecto.Query{}, [conn: %Plug.Conn{}], fn ->
+      42
+    end)
+    assert return_value == 42
+  end
+end
