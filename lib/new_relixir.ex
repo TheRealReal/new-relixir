d8d02e14aaa467903f464e28dfe74f555f281221
João Britto <jabcalves@gmail.com>
2017-12-10 18:42:08 -0200

    Restructure main application and mixfile according to Elixir 1.4+ standards.
    * Restore vanilla .gitignore
    * Keep `mix.lock` versioned
    * Move app initialization to NewRelixir.Application
    * Reformat mix.exs
    * Declare Ecto and Phoenix deps as `runtime: false`
    * Declare ExDoc dep as `only: :docs`

--- lib/new_relixir.ex
+++ lib/new_relixir.ex
@@ -1,29 +1,7 @@
 defmodule NewRelixir do
   @moduledoc """
-  Entry point for New Relixir OTP application.
+  New Relic instrumentation for Elixir apps.
   """
-
-  use Application
-
-  require Logger
-
-  @doc """
-  Application callback to start New Relixir.
-  """
-  @spec start(Application.app, Application.start_type) :: :ok | {:error, term}
-  def start(_type \\ :normal, _args \\ []) do
-    import Supervisor.Spec, warn: false
-
-    check_config()
-
-    children = [
-      worker(NewRelixir.Collector, []),
-      worker(NewRelixir.Polling, [&NewRelixir.Stats.pull/0])
-    ]
-
-    opts = [strategy: :one_for_one, name: NewRelixir.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
 
   @doc """
   Tells if New Relixir is configured correctly and pushing data to New Relic.
@@ -31,17 +9,5 @@ defmodule NewRelixir do
   @spec active? :: boolean
   def active? do
     Application.get_env(:new_relixir, :active)
-  end
-
-  defp check_config do
-    application_name = Application.get_env(:new_relixir, :application_name)
-    license_key = Application.get_env(:new_relixir, :license_key)
-    valid_config = String.valid?(application_name) && String.valid?(license_key)
-
-    if valid_config, do: Logger.info fn ->
-      "New Relixir set up for '#{application_name}'."
-    end
-
-    Application.put_env(:new_relixir, :active, valid_config)
   end
 end
