d929bee29ca78c36ed8d209a82c435eb13ff6e26
Adam Faur <adam.faur@gmail.com>
2018-10-08 12:39:33 -0700

    Add spec definitions (#76)
    Only one doc allowed per same name and arity fn
    Closes #68

--- lib/new_relixir/instrumenters/phoenix.ex
+++ lib/new_relixir/instrumenters/phoenix.ex
@@ -15,12 +15,18 @@ defmodule NewRelixir.Instrumenters.Phoenix do
   alias NewRelixir.{CurrentTransaction, Transaction, Utils}
 
   @doc """
-  Start callback for Phoenix controllers. Phoenix calls it once a request is routed,
-  before processing a controller action.
-
+  If first argument is the atom `:start` this is used as the Start callback for
+  Phoenix controllers. Phoenix calls it once a request is routed, before
+  processing a controller action.
   Returns the transaction name, formatted as `Controller#action`. This value is
   stored and passed along as the third argument to the `:stop` callback.
+
+  When called with the atom `:stop` this is used as the Stop callback for
+  Phoenix controllers. Phoenix calls it after the whole controller pipeline
+  finishes executing the routed action. You should additionally provide the
+  elapsed_time and the Transaction as the second and third arguments.
   """
+  @spec phoenix_controller_call(:start, %{}, %Plug.Conn{}) :: transaction :: binary()
   def phoenix_controller_call(:start, _compile_metadata, %{conn: conn}) do
     if NewRelixir.active? do
       transaction = Utils.transaction_name(conn)
@@ -29,10 +35,7 @@ defmodule NewRelixir.Instrumenters.Phoenix do
     end
   end
 
-  @doc """
-  Stop callback for Phoenix controllers. Phonix calls it after the whole controller
-  pipeline finishes executing the routed action.
-  """
+  @spec phoenix_controller_call(:stop, integer(), transaction :: binary()) :: :ok | nil
   def phoenix_controller_call(:stop, elapsed_time, transaction) do
     if NewRelixir.active? do
       elapsed_microseconds = System.convert_time_unit(elapsed_time, :native, :microsecond)
